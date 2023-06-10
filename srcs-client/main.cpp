6aeb60287a60b56cbde955000bc65b61f362c07d
Nathan <58692289+NATHAN76543217@users.noreply.github.com>
2023-06-10 02:03:05 +0200

    Start to design the client

--- srcs-client/main.cpp
+++ srcs-client/main.cpp
@@ -100,29 +100,29 @@ int main(int ac, char **av)
 
 	using namespace ftxui;
 
-	auto summary = [&] {
-		auto content = vbox({
-			hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-			hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-			hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-			hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-		});
-		return window(text(L" Summary "), content);
-	};
+	// auto summary = [&] {
+	// 	auto content = vbox({
+	// 		hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
+	// 		hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
+	// 		hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
+	// 		hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
+	// 	});
+	// 	return window(text(L" Summary "), content);
+	// };
 
-	auto document =  //
-		vbox({
-			hbox({
-				summary(),
-				summary(),
-				summary() | flex,
-			}),
-			summary(),
-			summary(),
-		});
+	// auto document =  //
+	// 	vbox({
+	// 		hbox({
+	// 			summary(),
+	// 			summary(),
+	// 			summary() | flex,
+	// 		}),
+	// 		summary(),
+	// 		summary(),
+	// 	});
 
-	// Limit the size of the document to 80 char.
-	document = document | size(WIDTH, LESS_THAN, 80);
+	// // Limit the size of the document to 80 char.
+	// document = document | size(WIDTH, LESS_THAN, 80);
 
 	// auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
 	// Render(screen, document);
@@ -130,20 +130,20 @@ int main(int ac, char **av)
 // 	auto screen = ScreenInteractive::Fullscreen();
 
 //   int tab_index = 0;
-  int main_menu_index = 0;
+//   int main_menu_index = 0;
 //   std::vector<std::string> tab_entries = {
-//       "htop", "color", "spinner", "gauge", "compiler", "paragraph",
+// //       "htop", "color", "spinner", "gauge", "compiler", "paragraph",
+// //   };
+//   std::vector<std::string> main_menu = {
+//       "connect", "client config", "client info"
 //   };
-  std::vector<std::string> main_menu = {
-      "connect", "client config", "client info"
-  };
 
 //     Elements line1;
 // 	line1.push_back(text("line1") | bold | hcenter);
 //   auto tab_selection =
 //       Menu(&tab_entries, &tab_index, MenuOption::VerticalAnimated());
-  auto tab_commands =
-      Menu(&main_menu, &main_menu_index, MenuOption::VerticalAnimated()) | center;
+//   auto tab_commands =
+//       Menu(&main_menu, &main_menu_index, MenuOption::VerticalAnimated()) | center;
 // 	auto shell_placeholder = vbox({
 // 		line1
 // 	});
@@ -190,33 +190,126 @@ int main(int ac, char **av)
 	
 // 	screen.Loop(main_renderer);
 	
-	  auto screen = ScreenInteractive::Fullscreen();
+// 	  auto screen = ScreenInteractive::Fullscreen();
  
-  auto middle = Renderer([] { return text("middle") | size(HEIGHT, EQUAL, 20) | center ; });
-  auto left = Renderer([] { return text("Left") | center; });
-  auto right = Renderer([] { return text("right") | center; });
-  auto top = Renderer([] { return text("top") | center; });
-  auto bottom = Renderer([] { return text("bottom") | center; });
+//   auto leftcontainer = Renderer([] { return text("left container") | size(HEIGHT, EQUAL, 20) | center ; });
+//   auto left = Renderer([] { return text("Left") | center; });
+//   auto right = Renderer([] { return text("right") | center; });
+//   auto top2 = Renderer([] { return text("top2") | center; });
+//   auto top3 = Renderer([] { return text("top3") | center; });
+//   auto top = Renderer([] { return text("top") | center; });
+//   auto middle = Renderer([] { return text("middle") | size(HEIGHT, EQUAL, 20) | center ; });
+//   auto bottom = Renderer([] { return text("bottom") | center; });
  
-  int left_size = 20;
-  int right_size = 20;
-  int top_size = 10;
-  int bottom_size = 10;
+// //   int left_size = 20;
+//   int right_size = 20;
+//   int top_size = 10;
+// //   int bottom_size = 10;
  
-  auto container = middle;
-  container = ResizableSplitLeft(tab_commands, container, &left_size);
-  container = ResizableSplitRight(right, container, &right_size);
-  container = ResizableSplitTop(top, container, &top_size);
-  container = ResizableSplitBottom(bottom, container, &bottom_size);
+//   auto container_top = top;
+//   auto container_middle = middle;
+//   auto container_bottom = bottom;
+// //   container = ResizableSplitLeft(tab_commands, container, &left_size);
+//   container_bottom = ResizableSplitRight(right, container_bottom, &right_size);
+//     container_middle = ResizableSplitBottom(bottom, container_middle, &top_size);
+//     container_top = ResizableSplitBottom(middle, container_top, &top_size);
+//     // container = ResizableSplitTop(top3, container, &top_size);
+// //   container = ResizableSplitBottom(bottom, container, &bottom_size);
  
-  auto renderer =
-      Renderer(container, [&] { return container->Render() | border; });
+//   auto renderer =
+//       Container::Vertical({ container_top->Render() | border, container_bottom->Render()});
  
-//   screen.Print(renderer);
-  screen.Loop(renderer);
+// //   screen.Print(renderer);
+//   screen.Loop(renderer);
 
-}
+// }
+//     return (EXIT_SUCCESS);
+using namespace ftxui;
+
+    int main_menu_index = 0;
+
+    std::vector<std::string> main_menu = {
+        "connect", "client config", "client info"
+    };
+
+    auto tab_commands =
+        Menu(&main_menu, &main_menu_index, MenuOption::VerticalAnimated()) | center;
+
+
+    auto screen = ScreenInteractive::Fullscreen();
+
+    // top
+    auto topBar = Renderer([] { return text("Taskmaster Client (v 0.0.0)") | center; });
+
+	# define TEXT_COLOR Color(200, 200, 200)
+	# define BORDER_COLOR Color::White
+	
+    // left
+    auto jobList = Renderer([] { return window(text("Jobs"),
+			text("Jobs content") | center | color(TEXT_COLOR) | bgcolor(Color(0, 0, 0)))
+		| color(Color(200,0,0)); });
+
+	auto serverTitle = Renderer([] { return text("Server Status content") | center;  });
+    Component serverContainer = Container::Vertical({
+		serverTitle,
+		serverTitle,
+	});
+    auto serverWindow = Renderer([] { return window(text("Server Status"), 
+			paragraphAlignLeft("Server Status content") | color(TEXT_COLOR) | bgcolor(Color(0, 0, 0)) )
+		| color(Color(200,200,200)); });
+// | borderStyled(LIGHT, Color::White)
+
+    auto remoteShell = Renderer([] { return 
+		window(
+			text("Remote Shell") | color(TEXT_COLOR),
+			text("TODO Shell component...") | color(TEXT_COLOR)
+		) | color(BORDER_COLOR);
+	});
+
+    // right
+    auto configEditor = Renderer([] { return
+    	window(
+			text("Config Editor") | color(TEXT_COLOR) | center,
+			paragraphAlignLeft("TODO Config component...") | color(TEXT_COLOR) | bgcolor(Color(0, 0, 0))
+		) | color(BORDER_COLOR);
+	});
+
+	auto outputLog = Renderer([] { return
+	window(
+		text("Output Log") | color(TEXT_COLOR),
+		text("TODO Output component...") | color(TEXT_COLOR) | bgcolor(Color::Black)
+	) | color(BORDER_COLOR); });
+
+
+    int serverStatusSize = 10;
+    int outputLogSize = 10;
+    int remoteShellSize = 10;
+
+
+    auto leftContainer = jobList;
+    leftContainer = ResizableSplitBottom(serverWindow, leftContainer, &serverStatusSize) | color(Color::Black) | borderEmpty;// borderStyled(LIGHT, Color::Red);
+    leftContainer = ResizableSplitBottom(remoteShell, leftContainer, &remoteShellSize) | color(Color::Black) | borderEmpty;
+
+    auto rightContainer = configEditor;
+    rightContainer = ResizableSplitBottom(outputLog, rightContainer, &outputLogSize) | color(Color::Black)  | borderEmpty;
+
+    int rightContainerSize = 10;
+    auto contentContainer = leftContainer;
+    contentContainer = ResizableSplitRight(rightContainer, contentContainer, &rightContainerSize) | color(Color::Black)  | borderEmpty;
+    
+
+    int contentContainerSize = 1;
+    auto mainContainer = topBar;
+    mainContainer = ResizableSplitBottom(contentContainer, mainContainer, &contentContainerSize) | borderEmpty;
+
+    auto renderer =
+        Renderer(mainContainer, [&] { return mainContainer->Render() | borderStyled(ROUNDED, Color::White) | vscroll_indicator | bgcolor(Color::Black); });
+
+    //   screen.Print(renderer);
+    screen.Loop(renderer);
+
     return (EXIT_SUCCESS);
+}
 }
 
 
